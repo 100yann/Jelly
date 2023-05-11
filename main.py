05be61ab3163dbc693041eec906153b1474b9fb3
Stoyan Kolev <stoyan.d.kolev@gmail.com>
2023-05-11 18:13:58 +0300

    code improvements

--- main.py
+++ main.py
@@ -8,13 +8,11 @@ from tkinter import filedialog
 # os.chdir() - change directory to specified path
 # os.listdir() - list all files and subdirectories
 # os.remove(file) - removes file in current directory
-
-filename = 'C://Users/Stoyan/CodingProjects/Jelly/jelly.txt'
-creation_date = os.path.getctime(filename)
+# os.path.getctime(file dir) - returns time of creation of specified file
 
 root=tk.Tk()
 root.geometry('1280x720')
-# root.withdraw()
+root.config(pady=50)
 options_label = tk.Label(
     text='Choose an option'
 )
@@ -38,21 +36,33 @@ def del_files():
 
 
     def init_delete():
-        all_files = os.listdir(file_path)
-        for file in all_files:
-            creation_date = os.path.getctime(f'{file_path}/{file}')
-            readable_date = dt.datetime.fromtimestamp(creation_date).date()
-            if readable_date >= dt.date.today() - timedelta(days=int(user_input.get())):
-                print('yes')
-                os.remove(f'{file_path}/{file}')
+        root_dir = os.listdir(file_path)
+        for num, season in enumerate(root_dir):
+            if f'SEASON_{num}' in root_dir or f'season_{num}' in root_dir or f'Season_{num}' in root_dir:
+                all_episodes = os.listdir(file_path+f'/SEASON_{num}')
+                for episode in all_episodes:
+                    if "CREXXX" in episode:
+                        pass
+                    else:
+                        try:
+                            end_dir = file_path + f'/SEASON_{num}' + f'/{episode}/1_EXPORTS/1_MASTERS/3_FACEBOOK'
+                            all_exports = os.listdir(end_dir)
+                            for export in all_exports:
+                                creation_date = os.path.getctime(f'{end_dir}/{export}')
+                                readable_date = dt.datetime.fromtimestamp(creation_date).date()
+                                if readable_date <= dt.date.today() - timedelta(days=int(user_input.get())):
+                                    try:
+                                        os.remove(f'{end_dir}/{export}')
+                                    except PermissionError:
+                                        pass
+                        except FileNotFoundError:
+                            pass
 
     delete = tk.Button(
         text='Delete',
         command=init_delete
     ).pack()
 
-
-    
 
 
 
@@ -61,9 +71,6 @@ del_exports = tk.Button(
     command=del_files
 )
 del_exports.pack()
-
-
-
 
 
 root.mainloop()
