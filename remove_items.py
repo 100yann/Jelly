ad86652b26106ca2b7baee2e992643c45d1e85e1
100yann <stoyan.d.kolev@gmail.com>
2023-05-25 15:34:17 +0300

    add multipurpose function that allows for a second type of file to be deleted - preedits, stored in another folder

--- remove_items.py
+++ remove_items.py
@@ -6,29 +6,33 @@ from datetime import timedelta
 import os
 
 
-def deleteExports(path, date):
+def deleteFiles(path, date, type=str):
     root_dir = Path(path)
     removed_files = []
     for subdir in root_dir.iterdir():
         if 'SEASON' in subdir.name:
             open_season = root_dir.joinpath(subdir)
             for episode in open_season.iterdir():
-                if 'CREXXX' not in episode.name:
+                if 'CREXXX' not in episode.name and type == 'exports':
                     final_dir = root_dir / subdir / episode / '1_EXPORTS' / '1_MASTERS' / '3_FACEBOOK'
-                    try:
-                        for export in final_dir.iterdir():
-                            try:
-                                creation_date = os.path.getctime(final_dir/export)
-                                readable_date = dt.datetime.fromtimestamp(creation_date).date()
-                                if readable_date <= dt.date.today() - timedelta(days=int(date)):
-                                    os.remove(final_dir/export)
-                                    removed_files.append(export)
-                            except PermissionError:
-                                pass
-                    except FileNotFoundError:
-                        pass
+                elif 'CREXXX' not in episode.name and type == 'preedits':
+                    final_dir = root_dir / subdir / episode / '4_EXCHANGES' / '1_PRE-EDIT'
+                try:
+                    for export in final_dir.iterdir():
+                        try:
+                            creation_date = os.path.getctime(final_dir/export)
+                            readable_date = dt.datetime.fromtimestamp(creation_date).date()
+                            if readable_date <= dt.date.today() - timedelta(days=int(date)):
+                                os.remove(final_dir/export)
+                                removed_files.append(export)
+                        except PermissionError:
+                            pass
+                except FileNotFoundError:
+                    pass
     if len(removed_files) == 0:
         print('No matching files were found')
     else:
         for file in removed_files:
             print(f"Deleted {file}")
+
+
